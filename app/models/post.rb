94d9fd04b484690e93a705a78d9e76711cf02e11
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 20:11:21 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,6 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with MyValidator
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary, length: { in: 6..20 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
-end
-
-class MyValidator < ActiveModel::Validator
-  def validate(post)
-    unless post.title.includes? 'Won\'t Believe Secret Top [number] Guess'
-      record.errors[:post] << 'Needs to be clickbait-y!'
-    end
-  end
 end
