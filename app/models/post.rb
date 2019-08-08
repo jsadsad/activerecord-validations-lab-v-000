863cc068aff5fb523feda61b9d6e3e9379afa369
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 20:16:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: { in: 6..20 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
-  validates :clickbait
+  validate :clickbait
 
   private
   def clickbait
