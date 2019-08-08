2e4c5fe034fd10425838a1d4e95f59158aa5d55d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 20:13:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: { in: 6..20 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+
+  private
+  def clickbait
+    clickbait_arr = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    if clickbait_arr.none? do |i|
+      i.match(title)
+    end
+      self.errors[:title] << "You need to be more clickbait-y"
+    end
+  end
 end
