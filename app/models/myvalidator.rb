0e697c32b9ebc5d105fb2fd44b5d3fda35acc06f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 20:08:52 +0000

    Automatically backed up by Learn

--- app/models/myvalidator.rb
+++
@@ -1,7 +0,0 @@
-class MyValidator < ActiveModel::Validator
-  def validate(post)
-    unless post.title.includes? 'Won\'t Believe Secret Top [number] Guess'
-      record.errors[:post] << 'Needs to be clickbait-y!'
-    end
-  end
-end
