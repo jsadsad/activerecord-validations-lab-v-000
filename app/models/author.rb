753379fc6e2f2d9344c14832067b36f8f908d385
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 19:52:06 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :phone_number, length: {in: 10..15}
+  validates :phone_number, length: {in: 10..15}, uniqueness: true
+
 end
