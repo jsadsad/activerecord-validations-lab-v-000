9724136dc9f5306e6e02d23a7671494e46bfd8ae
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 19:55:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { minimum: 8 } 
+  validates :content, length: { minimum: 8 }
+  validates :summary, length: { in: 6..20 }
 end
